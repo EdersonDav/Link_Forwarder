8d4c5e06a8a8cd7b9e9d6fe73758b74a679220b6
Ederson Davi <silva.edersonqueiroz@gmail.com>
2020-07-03 22:32:12 -0300

    Create messages e conection mongodb

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
 import express from "express";
-
+import mongoose from "mongoose";
 const app = express();
+const PORT = 5000;
+
+const loinkSchema = new mongoose.Schema({
+  clicks: Number,
+  title: String,
+  description: String,
+  url: String,
+});
+
+//Criando conexão com o banco
+mongoose.connect("mongodb://localhost/links", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+//Mensagem para evento de erro
+db.on("error", () => {
+  console.log("Houve um erro");
+});
+
+//Mensagem que mostra apenas uma vez
+db.once("open", () => {
+  console.log("Banco conectado");
+});
 
 app.get("/", (req, res) => {
   res.json({ message: "Hello World" });
 });
 
-app.listen(5000, () => {
+app.listen(PORT, () => {
   console.log("Server Rurning port 5000");
 });
